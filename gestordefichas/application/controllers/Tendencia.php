b8242666c7df6403d5872a2655b0f332b5fc4e3f
Camilla S. Reis <camilla.sreis2@gmail.com>
2024-02-12 02:28:49 -0300

    Solicitações para controller e model de TENDENCIA resolvidas.  Todos os métodos foram testados e 'conferirTendencia()' e 'validarTendencia()' foram organizados no script em uma seção de métodos de apoio, ou seja, que só existem nas models para garantir funcionamento do CRUD

---
+++ gestordefichas/application/controllers/Tendencia.php
@@ -0,0 +1,108 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Tendencia extends CI_Controller{
+    private $json;
+    private $resultado;
+    
+    private $idTend;
+    private $tendencia;
+
+    //Getters e Setters
+    public function getIdTend(){
+        return $this->idTend;
+    }
+    public function setIdTend($idTendFront){
+        $this->idTend = $idTendFront;
+    }
+    public function getTendencia(){
+        return $this->tendencia;
+    }
+    public function setTendencia($tendenciaFront){
+        $this->tendencia = $tendenciaFront;    
+    }
+
+    public function inserirTendencia(){
+        $json = file_get_contents('php://input');
+        $resultado = json_decode($json);
+        $lista = array('tendencia' => '0');
+
+        if(verificarParam($resultado, $lista) == 1){
+            $this->setTendencia($resultado->tendencia);
+
+            if(strlen($this->getTendencia()) == 0){
+                $retorno = array('codigo' => 16,
+                                 'msg'    => 'Tendência não informada');
+            }elseif(is_numeric($this->getTendencia()) == True){
+                $retorno = array('codigo' => 17,
+                                 'msg'    => 'Tendência não condiz com o permitido');
+            }else{
+                $this->load->model('M_Tendencia');
+                $retorno = $this->M_Tendencia->inserirTendencia($this->getTendencia());
+            }
+        }else{
+            $retorno = array('codigo' => 999,
+                             'msg'    => 'Campos do front não condizem com o permitido');
+        }
+            echo json_encode($retorno);
+    }
+
+    public function alterarTendencia(){
+        $json = file_get_contents('php://input');
+        $resultado = json_decode($json);
+        $lista = array('idTend'    => '0',
+                       'tendencia' => '0');
+    
+        if(verificarParam($resultado, $lista) == 1){
+            $this->setIdTend($resultado->idTend);
+            $this->setTendencia($resultado->tendencia);
+
+            if(strlen($this->getIdTend()) == 0){
+                $retorno = array('codigo' => 16,
+                                 'msg'    => 'Tendência não informada');
+            }elseif(is_numeric($this->getIdTend()) == False) {
+                $retorno = array('codigo' => 8,
+                                 'msg'    => 'ID não condiz com o permitido');
+            }elseif(strlen($this->getTendencia()) == 0){
+                $retorno = array('codigo' => 16,
+                                 'msg'    => 'Tendência não informada');
+            }elseif(is_numeric($this->getTendencia()) == True){
+                $retorno = array('codigo' => 17,
+                                 'msg'    => 'Tendência não condiz com o permitido');
+            }else{
+                $this->load->model('M_Tendencia');
+                $retorno = $this->M_Tendencia->alterarTendencia($this->getIdTend(), $this->getTendencia());
+            }
+        }else{
+            $retorno = array('codigo' => 999,
+                             'msg'    => 'Campos do front não condizem com o permitido');
+        }
+            echo json_encode($retorno);
+    }
+
+    public function apagarTendencia(){
+        $json = file_get_contents('php://input');
+        $resultado = json_decode($json);
+        $lista = array('idTend' => '0');
+
+        if(verificarParam($resultado, $lista) == 1){
+            $this->setIdTend($resultado->idTend);
+        
+            if($this->getIdTend() == ""){
+                $retorno = array('codigo' => 14,
+                                'msg'   => 'Nenhum dado selecionado');
+            }elseif(is_numeric($this->getIdTend()) == False){
+                $retorno = array('codigo' => 17,
+                                'msg' => 'Tendência não condiz com o permitido');
+            }else{
+                $this->load->model('M_Tendencia');
+                $retorno = $this->M_Tendencia->apagarTendencia($this->getIdTend());
+            }
+        }else{
+            $retorno = array('codigo' => 999,
+                           'msg'    => 'Campos do front não condizem com o permitido');
+       }
+       echo json_encode($retorno);
+    }
+}   
+
---
+++ gestordefichas/application/models/M_Tendencia.php
@@ -0,0 +1,100 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class M_Tendencia extends CI_model{
+    public function inserirTendencia($tendencia){
+        $retornoTendencia = $this->conferirTendencia($tendencia);
+        if($retornoTendencia['codigo'] == 1){
+            $dados = array('codigo' => 18,
+                           'msg'    => $retornoTendencia['msg']);
+        }else{
+            $sql = "insert into TENDENCIAS (tendencia) values ('$tendencia')";
+            $this->db->query($sql);
+            if($this->db->affected_rows() > 0){
+                $dados = array('codigo' => 7,
+                               'msg'    => 'Cadastro concluído');
+            }else{
+                $dados = array('codigo' => 6,
+                               'msg'    => 'Houve um problema ao efetuar função');
+            }
+        }
+        return $dados;
+    }
+
+    public function alterarTendencia($idTend, $tendencia){
+        $retornoID = $this->validarTendencia($idTend);
+        if($retornoID['codigo'] == 1){
+            $retornoTendencia = $this->conferirTendencia($tendencia);
+            if($retornoTendencia['codigo'] == 1){
+                $dados = array('codigo' => 18,
+                                'msg' => $retornoTendencia['msg']);
+            }else{
+                $sql = "update TENDENCIAS set tendencia = '$tendencia' where id_tend = $idTend";            
+
+                $this->db->query($sql);
+                if($this->db->affected_rows() > 0){
+                    $dados = array('codigo' => 19,
+                                   'msg'    => 'Tendência alterada');
+                }else{
+                    $dados = array('codigo' => 6,
+                                   'msg'    => 'Houve algum problema ao efetuar função');
+                }
+            }
+        }else{
+            $dados = array('codigo' => 11,
+                           'msg'    => $retornoID['msg']);
+        }
+        return $dados;
+    }
+
+    public function apagarTendencia($idTend){
+        $retornoTendencia = $this->validarTendencia($idTend);
+        if($retornoTendencia['codigo'] == 2){
+            $dados = array('codigo' => 11,
+                            'msg' => $retornoTendencia['msg']);
+        }else{
+            $sql = "delete from TENDENCIAS where id_tend = $idTend";
+
+            $this->db->query($sql);
+            if($this->db->affected_rows() > 0){
+                $dados = array('codigo' => 15,
+                                'msg'    => 'Dado apagado');
+            }else{
+                $dados = array('codigo' => 6,
+                                'msg'    => 'Houve um problema ao efetuar função');
+            }
+        }
+        return $dados;
+    }
+
+    #   MÉTODOS DE APOIO
+    public function conferirTendencia($tendencia){
+        $sql = "select * from TENDENCIAS where tendencia = '$tendencia'";
+        $retorno = $this->db->query($sql);
+
+        if($retorno->num_rows() > 0){
+            $dados = array('codigo' => 1,
+                           'msg'    => 'Tendência já cadastrada');
+        }else{
+            $dados = array('codigo' => 2,
+                           'msg'    => 'Nenhum dado encontrado');
+        }
+        return $dados;
+    }
+
+    public function validarTendencia($idTend){
+        $sql = "select * from TENDENCIAS where id_tend = $idTend";
+        
+        $retorno = $this->db->query($sql);
+        if($retorno->num_rows() > 0){
+            $dados = array('codigo' => 1,
+                           'msg'    => 'Tendência já cadastrada');
+        }else{
+            $dados = array('codigo' => 2,
+                           'msg'    => 'Nenhum dado encontrado');
+        }
+        return $dados;
+    }
+}
+
+?>
