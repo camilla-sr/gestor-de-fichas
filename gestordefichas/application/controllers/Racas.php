30b015b34ea3971913ae8a25fda7eaeca62e75bd
Camilla S. Reis <camilla.sreis2@gmail.com>
2024-02-11 23:10:22 -0300

    Controller e model de RAÇAS zeradas e testadas, métodos de apagar são físicos

--- gestordefichas/application/controllers/Racas.php
+++ gestordefichas/application/controllers/Racas.php
@@ -62,5 +62,90 @@ class Racas extends CI_Controller{
         }
         echo json_encode($retorno);
     }
+
+    public function consultarRaca(){
+        $json = file_get_contents('php://input');
+        $resultado = json_decode($json);
+        $lista = array('idRaca'         => '0',
+                        'raca'          => '0',
+                        'deslocamento'  => '0');
+        if(verificarParam($resultado, $lista) == 1){
+            $this->setIdRaca($resultado->idRaca);
+            $this->setRaca($resultado->raca);
+            $this->setDeslocamento($resultado->deslocamento);
+
+            if(is_numeric($this->getIdRaca()) == False && $this->getIdRaca() == 0){
+                $retorno = array('codigo' => 8,
+                                'msg' => 'Id não condiz com o permitido');
+            }elseif(is_string($this->getRaca()) == False && $this->getRaca() == ""){
+                $retorno = array('codigo' => 3,
+                                'msg' => 'Nome da raça não condiz com o permitido');
+            }elseif(is_float($this->getDeslocamento()) == False && $this->getDeslocamento() == 0){
+                $retorno = array('codigo' => 4,
+                                'msg' => 'Deslocamento não condiz com o permitido');
+            }else{
+                $this->load->model('M_Raca');
+                $retorno = $this->M_Raca->consultarRaca($this->getIdRaca(), $this->getRaca(), $this->getDeslocamento());
+            }
+        }else{
+            $retorno = array('codigo' => 999,
+                            'msg' => 'Campos do front diferentes dos requisitados.');
+        }
+        echo json_encode($retorno);
+    }
+
+    public function alterarRaca(){
+        $json = file_get_contents('php://input');
+        $resultado = json_decode($json);
+        $lista = array('idRaca'         => '0',
+                        'raca'          => '0',
+                        'deslocamento'  => '0');
+        if(verificarParam($resultado, $lista) == 1){
+            $this->setIdRaca($resultado->idRaca);
+            $this->setRaca($resultado->raca);
+            $this->setDeslocamento($resultado->deslocamento);
+
+            if($this->getIdRaca() != "" && $this->getRaca() == "" && $this->getDeslocamento() == ""){
+                $retorno = array('codigo' => 13,
+                                'msg' => 'Informe dado para alteração');
+            }elseif(is_numeric($this->getIdRaca()) == False){
+                $retorno = array('codigo' => 8,
+                                'msg' => 'Id não condiz com o permitido');
+            }elseif(is_string($this->getRaca()) == False && strlen($this->getRaca()) == 0){
+                $retorno = array('codigo' => 3,
+                                'msg' => 'Nome da raça não condiz com o permitido');
+            }elseif(is_float($this->getDeslocamento()) == False && $this->getDeslocamento() == ""){
+                $retorno = array('codigo' => 4,
+                                'msg' => 'Deslocamento não condiz com o permitido');
+            }else{
+                $this->load->model('M_Raca');
+                $retorno = $this->M_Raca->alterarRaca($this->getIdRaca(), $this->getRaca(), $this->getDeslocamento());
+            }
+        }else{
+            $retorno = array('codigo' => 999,
+                            'msg' => 'Campos do front diferentes dos requisitados.');
+        }
+        echo json_encode($retorno);
+    }
+
+    public function apagarRaca(){
+        $json = file_get_contents('php://input');
+        $resultado = json_decode($json);
+        $lista = array('idRaca'         => '0');
+        if(verificarParam($resultado, $lista) == 1){
+            $this->setIdRaca($resultado->idRaca);
+            if(is_numeric($this->getIdRaca()) == False){
+                $retorno = array('codigo' => 14,
+                                'msg' => 'Nenhum dado selecionado');
+            }else{
+                $this->load->model('M_Raca');
+                $retorno = $this->M_Raca->apagarRaca($this->getIdRaca());
+            }
+        }else{
+            $retorno = array('codigo' => 999,
+                            'msg' => 'Campos do front diferente dos requisitados.');
+        }
+        echo json_encode($retorno);
+    }
 }
 ?>
---
+++ gestordefichas/application/models/M_Racas.php
@@ -0,0 +1,119 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class M_Raca extends CI_Model{
+    public function inserirRaca($raca, $deslocamento){
+        $retornoRaca = $this->conferirRaca($raca);
+        if($retornoRaca['codigo'] == 5){
+            $dados = array('codigo' => 5,
+                            'msg' => 'Raça já cadastrada');
+        }else{
+            $sql = "insert into RACAS (raca, deslocamento) values ('$raca', $deslocamento)";
+            $this->db->query($sql);
+            if($this->db->affected_rows() > 0){
+                $dados = array('codigo' => 7,
+                                'msg' => 'Cadastro concluído');
+            }else{
+                $dados = array('codigo' => 6,
+                                'msg' => 'Houve um problema ao efetuar função');
+            }
+        }
+        return $dados;
+    }
+
+    public function consultarRaca($idRaca, $raca, $deslocamento){
+        if($idRaca == "" and $raca == "" and $deslocamento == ""){
+            $sql = "select * from RACAS";
+        }elseif($idRaca != "" and $raca == "" and $deslocamento == ""){
+            $sql = "select * from RACAS where id_raca = $idRaca";
+        }elseif($idRaca == "" and $raca != "" and $deslocamento == ""){
+            $sql = "select * from RACAS where raca like '%$raca%'";
+        }elseif($idRaca == "" and $raca == "" and $deslocamento != ""){
+            $sql = "select * from RACAS where deslocamento = $deslocamento";
+        }
+        $retorno = $this->db->query($sql);
+        if($retorno->num_rows() > 0){
+            $dados = array('codigo' => 88,
+                            'dados' => $retorno->result());
+        }else{
+            $dados = array('codigo' => 11,
+                            'msg' => 'Nenhum dado encontrado');
+        }
+        return $dados;
+    }
+
+    public function alterarRaca($idRaca, $raca, $deslocamento){
+        $retornoRaca = $this->conferirRaca($idRaca);
+        if($retornoRaca['codigo'] == 1){
+            $dados = array('codigo' => 5,
+                            'msg' => 'Raça já cadastrada');
+        }else{
+            if($deslocamento == "" and $raca != ""){
+                $sql = "update RACAS set raca = '$raca' ";
+            }elseif($deslocamento != "" and $raca == ""){
+                $sql = "update RACAS set deslocamento = $deslocamento ";
+            }elseif($deslocamento != "" and $raca != ""){
+                $sql = "update RACAS set raca = '$raca', deslocamento = $deslocamento ";
+            }
+
+            $sql = $sql . "where id_raca = $idRaca";
+            $this->db->query($sql);
+            if($this->db->affected_rows() > 0){
+                $dados = array('codigo' => 12,
+                                'msg' => 'Dados alterados');
+            }else{
+                $dados = array('codigo' => 6,
+                                'msg' => 'Houve um problema ao efetuar função');
+            }
+        }
+        return $dados;
+    }
+
+    public function apagarRaca($idRaca){
+        $retornoRaca = $this->validarRaca($idRaca);
+        if($retornoRaca['codigo'] == 2){
+            $dados = array('codigo' => 11,
+                            'msg' => $retornoRaca['msg']);
+        }else{
+            $sql = "delete from RACAS where id_raca = $idRaca";
+            $this->db->query($sql);
+            if($this->db->affected_rows() > 0){
+                $dados = array('codigo' => 15,
+                                'msg' => 'Dado apagado');
+            }else{
+                $dados = array('codigo' => 6,
+                                'msg' => 'Houve um problema ao efetuar função');
+            }
+        }
+        return $dados;
+    }
+
+
+    #MÉTODOS DE APOIO
+    public function conferirRaca($raca){
+        $sql = "select * from RACAS where raca = '$raca'";
+        $retorno = $this->db->query($sql);
+        if($retorno->num_rows() > 0){
+            $dados = array('codigo' => 1,
+                            'msg' => 'Raça válida');
+        }else{
+            $dados = array('codigo' => 2,
+                            'msg' => 'Nenhum dado encontrado');
+        }
+        return $dados;
+    }
+
+    public function validarRaca($idRaca){
+        $sql = "select * from RACAS where id_raca = $idRaca";
+        $retorno = $this->db->query($sql);
+        if($retorno->num_rows() > 0){
+            $dados = array('codigo' => 1,
+                            'msg' => 'Id válido');
+        }else{
+            $dados = array('codigo' => 2,
+                            'msg' => 'Nenhum dado encontrado');
+        }
+        return $dados;
+    }
+}
+?>
